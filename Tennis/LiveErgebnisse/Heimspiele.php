28f29b2dfa27024374ede9fa2f187e3c2f0a47c8
FabioKron <fabio.kron@web.de>
2022-04-11 11:01:26 +0200

    test heimspiele.php

--- Tennis/LiveErgebnisse/Heimspiele.php
+++ Tennis/LiveErgebnisse/Heimspiele.php
@@ -1,73 +1,88 @@
-<?php
-  require_once "simple_html_dom.php";
-    
-$html = file_get_html("https://www.rlp-tennis.de/liga/vereine/verein/begegnungen/v/21710.html?cHash=34811ff732377e3e5f0bd7f96c2a2ca5");
+<!DOCTYPE html>
+<html lang="de">
 
-$results = array();
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TCBW Herxheim | Heimspiele</title>
+    <link rel="stylesheet" type="text/css" href="heimspiele.css" />
 
-if (!empty($html)) {
+</head>
 
-    $div_class = "";
-    $i = 0;
+<body>
 
-    foreach ($html->find("tr") as $div_class) {
+    <?php
+    require_once "simple_html_dom.php";
         
-        $herxheim_ist_heim = false;
+    $html = file_get_html("https://www.rlp-tennis.de/liga/vereine/verein/begegnungen/v/21710.html?cHash=34811ff732377e3e5f0bd7f96c2a2ca5");
 
-        # Heimmannschaft
-        $home = $div_class->find(".home")[0]->plaintext;
+    $results = array();
 
-        if (strpos($home, "TC Blau-Weiss Herxheim") == false){
-            continue;
+    if (!empty($html)) {
+
+        $div_class = "";
+        $i = 0;
+
+        foreach ($html->find("tr") as $div_class) {
+            
+            $herxheim_ist_heim = false;
+
+            # Filter Heimmannschaft
+            $home = $div_class->find(".home")[0]->plaintext;
+
+            if (strpos($home, "TC Blau-Weiss Herxheim") == false){
+                continue;
+            }
+
+            # Datum
+            $results[$i]['date'] = $div_class->find(".date")[0]->plaintext;
+
+            # Gruppe
+            $results[$i]['group'] = $div_class->find(".group")[0]->plaintext;
+
+            # Gastmannschaft
+            $results[$i]['guest'] = $div_class->find(".guest")[0]->plaintext;
+            
+            $i++;
         }
-
-        # Datum
-        $results[$i]['date'] = $div_class->find(".date")[0]->plaintext;
-
-        # Gruppe
-        $results[$i]['group'] = $div_class->find(".group")[0]->plaintext;
-
-        # Gastmannschaft
-        $results[$i]['guest'] = $div_class->find(".guest")[0]->plaintext;
-        
-        $i++;
     }
-}
 
-print_r("<table style='border:2px solid black;'>");
-print_r("<tr>");
-
-print_r("<td>");
-print_r("Datum");
-print_r("</td>");
-
-print_r("<td>");
-print_r("Gruppe");
-print_r("</td>");
-
-print_r("<td>");
-print_r("Gastmannschaft");
-print_r("</td>");
-
-print_r("</tr>");
-
-foreach ($results as $res) {
+    print_r("<table>");
     print_r("<tr>");
 
     print_r("<td>");
-    print_r($res["date"]);
+    print_r("Datum");
     print_r("</td>");
 
     print_r("<td>");
-    print_r($res["group"]);
+    print_r("Gruppe");
     print_r("</td>");
 
     print_r("<td>");
-    print_r($res["guest"]);
+    print_r("Gastmannschaft");
     print_r("</td>");
 
     print_r("</tr>");
-}
-print_r("</table>");
 
-?>
+    foreach ($results as $res) {
+        print_r("<tr>");
+
+        print_r("<td>");
+        print_r($res["date"]);
+        print_r("</td>");
+
+        print_r("<td>");
+        print_r($res["group"]);
+        print_r("</td>");
+
+        print_r("<td>");
+        print_r($res["guest"]);
+        print_r("</td>");
+
+        print_r("</tr>");
+    }
+    print_r("</table>");
+
+    ?>
+
+</body>
