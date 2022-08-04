a53eb2f5bddb5f11ff1a93d9d334e2cddfffc17d
FabioKron <fabio.kron@web.de>
2022-08-04 12:40:38 +0200

    test Heimspiele.php

--- Tennis/LiveErgebnisse/Heimspiele.php
+++ Tennis/LiveErgebnisse/Heimspiele.php
@@ -53,8 +53,8 @@
     }
 
     function replaceGroup($s){
-        $s = str_replace("A-Klasse Gruppe 04", "U9 A-Klasse", $s);
-        $s = str_replace("Herren Offen LK 7.0-25.0", "Vereinspokal Herren", $s);
+        $s = str_replace("A-Klasse Gruppe 004", "U9 A-Klasse", $s);
+        $s = str_replace("Herren Offen LK 7,0-25,0", "Vereinspokal Herren", $s);
         print_r("rrrrrrr");
         return $s;
     }
